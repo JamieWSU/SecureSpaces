21e378529f2930172de678b89a9d085599e80806
Zeak469 <torreszeaks@gmail.com>
2020-02-22 11:02:24 -0800

    Memes.

--- PhoneMessaging/send_message.py
+++ PhoneMessaging/send_message.py
@@ -1,11 +1,10 @@
-from twilio.rest import Client
-message = raw_input("Enter your message: ") 
+from twilio.rest import Client 
+import apiKeys as keys
+message = input("Enter your message: ") 
 # Your Account SID from twilio.com/console
-account_sid = "AC0e9ca8edcd3824bc731e0cb7388659e0"
 # Your Auth Token from twilio.com/console
-auth_token  = "db02e938d12c30c3e7f278d118ed7bc3"
 
-client = Client(account_sid, auth_token)
+client = Client(keys.account_sid, keys.auth_token)
 
 message = client.messages.create(
     to="+15093080228",
