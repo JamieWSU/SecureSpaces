dc9af675cdf97842e9570de98317e95f8f661356
Ford Powers <fordepowers@gmail.com>
2020-02-22 18:39:17 -0800

    Clean up UI and add logic for disabled submit button

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Button, Card, ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+import { Image, Button, Form, Col } from 'react-bootstrap';
 import './App.css';
 
 
@@ -8,35 +8,72 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      file: null
+      file: null,
+      intruder: true
     }
     this.handleChange = this.handleChange.bind(this)
+    this.setAuthorized = this.setAuthorized.bind(this)
+    this.setIntruder = this.setIntruder.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(event) {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  setIntruder() {
+    this.setState({
+      ...this.state,
+      intruder: true,
+      name: undefined
+    });
+  }
+
+  setAuthorized() {
+    this.setState({
+      ...this.state,
+      intruder: false
+    });
   }
 
   handleChange(event) {
     this.setState({
+      ...this.state,
       file: URL.createObjectURL(event.target.files[0])
     })
   }
   render() {
+    console.log(this.state.intruder)
     return (
       <div className="App">
         <Image src={this.state.file} thumbnail />
-        <br />
-        <Image className='upload' src={"https://simpleicon.com/wp-content/uploads/cloud-upload-1.png"} />
-        <br />
-        <Button>
-          <input type="file" onChange={this.handleChange} name="file" id="file" class="inputfile" />
-          <label for="file">Upload Image</label>
-        </Button>
-        <hr />
-        <ButtonToolbar className={"radiobutton"}>
-          <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-            <ToggleButton value={1}>Intruder</ToggleButton>
-            <ToggleButton value={2}>Authorized</ToggleButton>
-          </ToggleButtonGroup>
-        </ButtonToolbar>
-      </div>
+        <Form>
+          <Col>
+            <br />
+            <Image className='upload' src={"https://image.freepik.com/free-icon/upload-document_318-8461.jpg"} />
+            <br />
+            <Button variant="dark">
+              <input type="file" onChange={this.handleChange} name="file" id="file" class="inputfile" />
+              <label for="file">Upload Image</label>
+            </Button>
+            <hr />
+            <Button onClick={this.setIntruder} variant={this.state.intruder ? "dark" : "outline-dark"}>Intruder</Button>
+            <Button onClick={this.setAuthorized} variant={this.state.intruder ? "outline-dark" : "dark"}>Authorized</Button>
+            <Form.Group>
+              {!this.state.intruder ? <div>
+                <br />
+                <Form.Control onChange={this.onChange} name="name" placeholder="Name" value={this.state.name} />
+              </div> : null}
+            </Form.Group>
+            <hr />
+            <Button variant="dark" size="lg" disabled={!this.state.file || (!this.state.name && !this.state.intruder) ? true : false}>Submit</Button>
+          </Col>
+        </Form>
+        <br/>
+      </div >
     );
   }
 }
